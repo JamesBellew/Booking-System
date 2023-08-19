21931c321e7c2c6518199a09e75f4e4f128af0b0
James Bellew <11jamesbellew@gmail.com>
2023-08-19 13:20:47 +0100

    Added calender with time booking slots conditionaly rendering based on date picked

--- Booking-System/src/App.tsx
+++ Booking-System/src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import './index.css'
 import { TextField } from './TextField'
 
 
@@ -11,7 +10,7 @@ const App: React.FC = ()=> {
   return (
     <>
   <div>
-     hi
+
      <TextField text='hello' i={1} />
     </div>
     </>
--- Booking-System/src/TextField.tsx
+++ Booking-System/src/TextField.tsx
@@ -1,5 +1,10 @@
 import React from "react"
 import { useState,useRef } from 'react'
+import Calendar from 'react-calendar';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 interface Props{
     text:string;
     ok?:boolean;
@@ -7,11 +12,58 @@ interface Props{
     fn?: ()=> void;
 }
 export const TextField: React.FC<Props> = ()=>{
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const [count,setCount] = useState<number | null>(5);
-    const inputRef=  useRef<>();
+    const [value, onChange] = useState<Value>(new Date());
+    const [showTimePicker,updateTimePicker] = useState<boolean>(false);
+const userSelectedDate = () =>{
+    console.log('You just clicked a new date' );
+    updateTimePicker(true)
+}
+const userSelectOtherDate = () =>{
+updateTimePicker(false)
+}
     return(
+        <>
         <div>
-            <input ref={}></input>
-        </div>
+            {!showTimePicker &&
+        <Calendar onChange={onChange} value={value} onClickDay={userSelectedDate}/>
+            }
+      </div>
+      {
+
+      showTimePicker &&
+      <>
+      <div>
+        <br></br>
+        <h3 className="text-red-700 underline text-3xl font-bold underline">New Booking For {value.toLocaleString("en-US", options)}</h3>
+<button onClick={userSelectOtherDate}>Select Different Date</button>
+<br></br>
+
+<br></br>
+<br></br>
+      </div>
+      <div>
+<button>9:00</button>
+<button>9:30</button>
+<button>10:00</button>
+<button>10:30</button>
+<button>11:00</button>
+<button>11:30</button>
+<button>12:00</button>
+</div>
+<div>
+<button>12:30</button>
+<button>1:00</button>
+<button>1:30</button>
+<button>02:00</button>
+<button>02:30</button>
+<button>03:00</button>
+<button>03:30</button>
+</div>
+
+</>
+}
+      </>
     );
 };
