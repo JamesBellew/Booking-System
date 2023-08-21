f86bd91d09e9950b811fd88347fa4722f1024536
James Bellew <11jamesbellew@gmail.com>
2023-08-21 23:57:30 +0100

    now dispalys multiple bookings and deletion works

--- Booking-System/src/Bookings.tsx
+++ Booking-System/src/Bookings.tsx
@@ -9,20 +9,40 @@ localStorage.setItem("students", string)
 const savedBookingsJSON = localStorage.getItem('bookings');
 const savedBookings = JSON.parse(savedBookingsJSON);
      const [bookings,updateBookings] = useState<string | null| object>([localStorage.getItem('bookings')]);
-    console.log(bookings);
-    // const bookings = localStorage.getItem('bookings');
-    const deleteBooking = (bookings:any) =>{
-localStorage.clear();
-updateBookings(null);
-    }
+     const formatDate = (date:any) => {
+        return new Date(date).toLocaleDateString();
+      };
+    
+      const formatTime = (time:any) => {
+        return new Date(time).toLocaleTimeString();
+      };
+//     const deleteBooking = (bookings:any) =>{
+// localStorage.clear();
+// updateBookings(null);
+//     }
+     const deleteBooking = (bookingToDelete:any) => {
+    // Filter out the booking to delete
+    const updatedBookings = savedBookings.filter(booking => (
+      booking.day !== bookingToDelete.day || booking.time !== bookingToDelete.time
+    ));
+
+    // Update local storage with the modified array
+    localStorage.setItem('bookings', JSON.stringify(updatedBookings));
+
+    // Update state to reflect the change
+    // setSavedBookings(updatedBookings);
+  };
 return(
     <>
 
     <h4>Your Bookings</h4>
-    {savedBookings &&
-    <li>hel<button onClick={()=>deleteBooking(bookings)}>Delete</button></li>
-
-}
+    {savedBookings && savedBookings.map((booking:any, index:any) => (
+    <p key={index}>
+         {formatDate(booking.day) + " "} 
+      at  {booking.time + "    "}
+        <button onClick={() => deleteBooking(booking)}>Delete</button>
+    </p>
+))}
     {/* {toLocaleString("en-US", options)} */}
     </>
 )
